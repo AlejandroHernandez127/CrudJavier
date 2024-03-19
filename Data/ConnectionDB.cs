32d2ee4321b1d83c5145767a22eee1ec3c7d7fb5
Hernandez Loyola Erick Gael <eghernandezl@televisa.com.mx>
2024-03-19 11:29:54 -0600

    Se agrego la conexionDB y se crearon los models empresa,operadores y la carpeta de servicios con Operadores y empresa

---
+++ Data/ConnectionDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OperadoresAplicacion.Data
+{
+    public class ConnectionDB
+    {
+        private static SqlConnection connection = null;
+        private ConnectionDB()
+        {
+
+        }
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                if (connection == null)
+                {
+                    string cnx = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+                    connection = new SqlConnection(cnx);
+                    connection.Open();
+                }
+                return connection;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static void ClaseConnection()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
---
+++ services/Operadores.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace OperadoresAplicacion.services
+{
+    public class Operadores
+    {
+        private SqlCommand command = null;
+
+        public void CreateOperadores(
+            string Nombre, 
+            int Edad, 
+            int Salario, 
+            DateTime Fecha_Nacimiento,
+            int IdEmpresa)
+        {
+            try
+            {
+                command = new SqlCommand("nameStoreProcedure", Data.ConnectionDB.GetConnection());
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                //command = "@accion" + 'Create'
+                //      = 
+
+                //parameters
+                command.Parameters.AddRange(new SqlParameter[]
+                {
+                  new SqlParameter("@Nombre", Nombre),
+                  new SqlParameter("@Edad", Edad),
+                  new SqlParameter("@Salario", Salario),
+                  new SqlParameter("@Fecha_Nacimiento", Fecha_Nacimiento),
+                  new SqlParameter("@IdEmpresa", IdEmpresa)
+                });
+
+                //Execute
+                command.ExecuteNonQuery();
+
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}

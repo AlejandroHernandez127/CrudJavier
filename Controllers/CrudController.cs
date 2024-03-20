d344bbaf9ab8e8d5a27ab577c5cbd420012e0716
Hernandez Loyola Erick Gael <eghernandezl@televisa.com.mx>
2024-03-20 08:38:49 -0600

    Se creo la vista para agregar un Operador

--- Controllers/CrudController.cs
+++ Controllers/CrudController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +13,38 @@ namespace OperadoresAplicacion.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            ProjectMVC.Logica.Services.Empresa empresa = new ProjectMVC.Logica.Services.Empresa();
+            ViewBag.ListEmpresa = empresa.GetEmpresas();
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateOperadores()
+        {
+            ProjectMVC.Logica.Services.Empresa empresa = new ProjectMVC.Logica.Services.Empresa();
+              ViewBag.ListEmpresa = empresa.GetEmpresas();
+
+            ProjectMVC.Logica.Services.Operadores operadores = new ProjectMVC.Logica.Services.Operadores();
+
+            var Nombre = Request.Form["Nombre"];
+            var Edad = Convert.ToInt32(Request.Form["Edad"]);
+            var Salario = Convert.ToInt32(Request.Form["Salario"]);
+            var Fecha_Nacimiento = Convert.ToDateTime(Request.Form["Fecha_Nacimiento"]);
+            var IdEmpresa= Convert.ToInt32(Request.Form["IdEmpresa"]);
+
+            operadores.CreatedOperadores(Nombre,
+                                         Edad,
+                                         Salario,
+                                         Fecha_Nacimiento,
+                                         IdEmpresa);
+
+
+            return View("Index");
         }
     }
 }
